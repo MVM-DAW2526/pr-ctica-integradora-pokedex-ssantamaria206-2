05fbe393a77465ab7eb0c1db0bc21e6ff2436142
Salva <ssantamaria@institutmvm.cat>
2026-04-09 21:34:10 +0200

    Cartas pokemon con mas forma

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ fetchPokemonList().then(async (Pokemons) => {
     const promises = Pokemons.map(async (pokemon) => {
         const resposta = await fetch(pokemon.url);
         const data = await resposta.json();
-
         return {
             name: data.name,
             sprite: data.sprites.front_default
@@ -11,19 +10,14 @@ fetchPokemonList().then(async (Pokemons) => {
     });
 
     const results = await Promise.all(promises);
-
     results.forEach(pokemon => {
         const article = document.createElement("article");
-
         const foto = document.createElement("img");
         foto.src = pokemon.sprite;
-
         const name = document.createElement("p");
         name.textContent = pokemon.name;
-
         article.appendChild(foto);
         article.appendChild(name);
-
         CaixaPokemons.appendChild(article);
     });
 });
