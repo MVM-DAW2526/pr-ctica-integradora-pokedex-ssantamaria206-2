668a9c901a750aa2d37ef68ad15e22a4aea902e8
Salva <ssantamaria@institutmvm.cat>
2026-04-11 16:58:33 +0200

    missatge no configurat a pantalla detalls (a cambiar)

--- js/detalls.js
+++ js/detalls.js
@@ -2,14 +2,44 @@ const params = new URLSearchParams(window.location.search);
 const name = params.get("name");
 
 function crearFoto(data){
-    const MainArticle = document.querySelector(".FotoPokemon");
+    const ZonaFoto = document.querySelector(".FotoPokemon");
     const foto = document.createElement("img");
     foto.src = data.sprites.front_default;
     const NombrePokemon = document.createElement("h2");
     NombrePokemon.textContent = data.name;
-    MainArticle.appendChild(foto);
-    MainArticle.appendChild(NombrePokemon);
+    ZonaFoto.appendChild(foto);
+    ZonaFoto.appendChild(NombrePokemon);
 }
+
+function crearStats(data){
+    const ZonaStats = document.querySelector(".StatsPokemon");
+    const missatge = document.createElement("p");
+    missatge.textContent = "No configurat";
+    ZonaStats.appendChild(missatge)
+}
+
+function crearHabilidades(data){
+    const ZonaHabilidades = document.querySelector(".HabilidadesPokemon");
+    const missatge = document.createElement("p");
+    missatge.textContent = "No configurat";
+    ZonaHabilidades.appendChild(missatge)
+}
+
+function crearMovimientos(data){
+    const ZonaMovimientos = document.querySelector(".MovimientosPokemon");
+    const missatge = document.createElement("p");
+    missatge.textContent = "No configurat";
+    ZonaMovimientos.appendChild(missatge)
+}
+
+function crearEvoluciones(data){
+    const ZonaEvoluciones = document.querySelector(".EvolucionesPokemon");
+    const missatge = document.createElement("p");
+    missatge.textContent = "No configurat";
+    ZonaEvoluciones.appendChild(missatge)
+}
+
+
 fetchPokemon(name).then(data => {
     crearFoto(data);
     crearStats(data);
