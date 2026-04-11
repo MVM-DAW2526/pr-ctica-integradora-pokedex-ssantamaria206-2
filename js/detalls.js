0efc7e23175af3f63003802d0adb0e1dbb776dbb
Salva <ssantamaria@institutmvm.cat>
2026-04-11 15:59:07 +0200

    Detalls.js con funciones (ma o meno)

--- js/detalls.js
+++ js/detalls.js
@@ -1,6 +1,7 @@
 const params = new URLSearchParams(window.location.search);
 const name = params.get("name");
-fetchPokemon(name).then(data => {
+
+function crearFoto(data){
     const MainArticle = document.querySelector(".FotoPokemon");
     const foto = document.createElement("img");
     foto.src = data.sprites.front_default;
@@ -8,4 +9,11 @@ fetchPokemon(name).then(data => {
     NombrePokemon.textContent = data.name;
     MainArticle.appendChild(foto);
     MainArticle.appendChild(NombrePokemon);
+}
+fetchPokemon(name).then(data => {
+    crearFoto(data);
+    crearStats(data);
+    crearHabilidades(data);
+    crearMovimientos(data);
+    crearEvoluciones(data);
 });
